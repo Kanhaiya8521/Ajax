7e03bec911c7a15c9ee18578ee7149e4f417f06e
Kanhaiya kumar <intern4@vama.app>
2023-04-07 18:03:05 +0530

    successfully completed the Mars Rover

--- Mars Rover/script.js
+++ Mars Rover/script.js
@@ -1,3 +1,33 @@
-$( function() {
-    $( "#datepicker" ).datepicker();
-});
+
+var input = $( "#datepicker" ).datepicker({dateFormat: 'yy-mm-dd'});
+var nasaImages = $("#nasa-images");
+$("p, button").click(function (e) {
+    // console.log(e);
+    e.preventDefault();
+
+    var date = input.val();
+    // console.log(date);
+    if(date === "") {
+        alert("Please enter a date");
+        return;
+    }
+    let url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" + date + "&api_key=Dof6zga8flRhR8Kar1xvRDckQf5XSstYnEBxWwWf";
+    console.log(url);
+
+    $.get(url, function (data) {
+        let photos = data.photos;
+        // console.log(photos);
+        if(photos.length === 0) {
+            alert("No photos available for this date");
+            return;
+        }
+
+        $("#nasa-images img").remove();
+
+        for(let photo of photos) {
+            nasaImages.append('<img src="' + photo.img_src + '" alt="' + photo.id + '" >');
+        }
+
+    });
+
+})
